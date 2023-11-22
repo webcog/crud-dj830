cd4de06564b7b9f2128ff4447041344147a97ec0
webcog <wajahatmurtaza144@gmail.com>
2023-11-22 20:43:29 +0500

    22-11-2023

--- core/views.py
+++ core/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Student
 # Create your views here.
 
 def index(request):
+    
+    if request.method == 'POST':
+        first_name = request.POST.get('fname')
+        last_name = request.POST.get('lname')
+        father_name = request.POST.get('fathername')
+        email = request.POST.get('email')
+        addressOne = request.POST.get('inputAddressOne')
+        addressTwo = request.POST.get('inputAddressTwo')
+        cnic = request.POST.get('cnic')
+        contact = request.POST.get('contact')
+
+        student = Student.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            father_name=father_name,
+            email=email,
+            addressOne=addressOne,
+            addressTwo=addressTwo,
+            cnic=cnic,
+            contact=contact
+        )
+        return redirect('viewStudent')
+
+
+
     return render(request, 'index.html')
 
 
